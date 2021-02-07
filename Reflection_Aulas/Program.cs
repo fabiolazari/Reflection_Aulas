95be1fcf475f7611afb92eba7d5d20e8d275aa22
Fabio Lazari Costa <lazari.developer@gmail.com>
2021-02-07 19:43:25 -0300

    AtualizandoProjeto.

---
+++ Reflection_Aulas/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Reflection_Aulas
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var objetoA = new A
+			{
+				Id = 100,
+				Descricao = "Valor descrição"
+			};
+
+			var objetoB = new B();
+
+			var propriedadeA = objetoA.GetType().GetProperties();
+
+			foreach (var propA in propriedadeA)
+			{
+				var propertyInfoB = objetoB.GetType().GetProperty(propA.Name);
+
+				if (propertyInfoB != null)
+				{
+					propertyInfoB.SetValue(objetoB, propA.GetValue(objetoA));
+				}
+			}
+
+
+			Console.WriteLine("====== Valor de A ======");
+			Console.WriteLine(objetoA.Id);
+			Console.WriteLine(objetoA.Descricao);
+
+
+			Console.WriteLine("====== Valor de A ======");
+			Console.WriteLine(objetoB.Id);
+			Console.WriteLine(objetoB.Descricao);
+
+			Console.ReadLine();
+		}
+	}
+
+	class A
+	{
+		public int Id { get; set; }
+		public string Descricao { get; set; }
+	}
+
+	class B
+	{
+		public int Id { get; set; }
+		public string Descricao { get; set; }
+	}
+}
